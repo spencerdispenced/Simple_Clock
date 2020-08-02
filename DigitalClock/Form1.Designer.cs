57f0df9e80df504c3a178892574776be8b1c8c03
Spencer Thomason <spencerdispenced@gmail.com>
2020-08-02 01:06:55 -0400

    Add files via upload

---
+++ DigitalClock/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace DigitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelClock = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelClock
+            // 
+            this.labelClock.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelClock.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClock.ForeColor = System.Drawing.Color.Lime;
+            this.labelClock.Location = new System.Drawing.Point(0, 0);
+            this.labelClock.Name = "labelClock";
+            this.labelClock.Size = new System.Drawing.Size(766, 353);
+            this.labelClock.TabIndex = 0;
+            this.labelClock.Text = "00:00:00 AM";
+            this.labelClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelDate
+            // 
+            this.labelDate.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.ForeColor = System.Drawing.Color.Lime;
+            this.labelDate.Location = new System.Drawing.Point(0, 307);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(766, 46);
+            this.labelDate.TabIndex = 1;
+            this.labelDate.Text = "Date";
+            this.labelDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(766, 353);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelClock);
+            this.Name = "Form1";
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelClock;
+        private System.Windows.Forms.Label labelDate;
+    }
+}
+
---
+++ DigitalClock/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalClock
+{
+    public partial class Form1 : Form
+    {
+        Timer T = new Timer();  // Create timer object
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            T.Interval = 1000;  // updates at every 1000ms, or every second
+            T.Tick += new EventHandler(UpdateLabels); // do this every update
+            T.Start(); // start the timer object running
+        }
+
+        private void UpdateLabels(object sender, EventArgs args)
+        {
+            labelClock.Text = DateTime.Now.ToString("hh:mm:ss tt");
+            labelDate.Text = DateTime.Now.ToString("MM/dd/yyyy");
+        }
+
+        
+    }
+}
